7eaba50f1d98d45699eb3b5998f4ab28a2c690d3
OlgaSS <olga.lole96@gmail.com>
2023-07-08 11:39:54 +0300

    feat: add main modules

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+import gulp from 'gulp';
+// Импорт путей
+import { path } from './gulp/config/path.js';
+
+// Передаем значения в глобальную переменную
+global.app = {
+    path: path,
+    gulp: gulp,
+}
